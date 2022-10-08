bf639d52aa4b88f4a55d93d31e1f6d47a8f9416d
mortuza62 <mortuza0608@gmail.com>
2022-10-08 19:44:35 +0600

    DOM-how to change inner text

--- DOM/dom.js
+++ DOM/dom.js
@@ -1,7 +1,25 @@
-const headerCollection = document.getElementById('fukingFriends');
-
-console.log(headerCollection.innerText);
+const headerCollection = document.getElementById('faltuFriends');
+// console.log(headerCollection.innerText);
 
 const para = document.getElementById('first-para');
+// console.log(para.innerText);
 
-console.log(para.innerText);
+
+// Change inner elements
+
+let header = document.getElementById('first-para');
+header.innerText = 'Ki likhbo ta ami jani na';
+// console.log(header.innerText);
+// console.log(header.innerHTML);
+
+
+// const allli = document.getElementsByTagName('li');
+
+// for(const li of allli);
+// {
+//     console.log(li.innerText);
+// }
+
+
+let ulClass = document.getElementsByClassName('haramiBondhu');
+console.log(ulClass);
